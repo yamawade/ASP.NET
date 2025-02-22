9dfb1ca24c5d0ec34e8b37edb30abfcda2ecb66c
yamawade <mariamawade69@gmail.com>
2025-02-22 01:22:41 +0000

    envoie message whatsapp

---
+++ AppAspGroupe12025/Controllers/MessageController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+using AppAspGroupe12025.Models;
+
+namespace AppAspGroupe12025.Controllers
+{
+    public class MessageController : Controller
+    {
+        // GET: Message
+        //public ActionResult Index()
+        //{
+        //    return View();
+        //}
+
+        private readonly Message _message;
+
+        public MessageController()
+        {
+            _message = new Message();
+        }
+
+        // Page d'accueil du formulaire
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> EnvoyerMessage(string numero, string message)
+        {
+            if (string.IsNullOrEmpty(numero) || string.IsNullOrEmpty(message))
+            {
+                return Content("Numéro ou message vide !");
+            }
+
+            string result = await _message.SendMessageAsync(numero, message);
+            return Content(result);
+        }
+    }
+}
