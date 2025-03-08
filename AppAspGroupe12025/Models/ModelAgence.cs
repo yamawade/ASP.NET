6778731c89268ec84db59cfc13d41a93e62854f3
yamawade <mariamawade69@gmail.com>
2025-03-08 01:40:32 +0000

    reporting

---
+++ AppAspGroupe12025/Models/ModelAgence.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AppAspGroupe12025.Models
+{
+    public class ModelAgence
+    {
+        
+        public string NineaAgence { get; set; }
+
+        public string AdresseAgence { get; set; }
+       
+        public float Longitude { get; set; }
+        
+        public float Latitude { get; set; }
+
+        public string RccmAgence { get; set; }
+    }
+}
