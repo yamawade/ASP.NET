66a3da0640755234246411c676660716af6a4937
yamawade <mariamawade69@gmail.com>
2025-05-31 12:48:51 +0000

    Mise à jour Program.cs : ajout DI, JsonOptions, AutoMapper, Swagger

--- AspCoreGroupe12025/Program.cs
+++ AspCoreGroupe12025/Program.cs
@@ -1,4 +1,7 @@
+using AspCoreGroupe12025.Entities;
 using AspCoreGroupe12025.Helpers;
+using AspCoreGroupe12025.Services;
+using System.Text.Json.Serialization;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -8,7 +11,19 @@ var env = builder.Environment;
 
 services.AddDbContext<DataContext>();
 
-services.AddControllers();
+services.AddControllers().AddJsonOptions(x =>
+{
+    // serialize enums as strings in API responses (e.g. Role)
+    x.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter());
+
+    // ignore omitted parameters on models to enable optional params (e.g. User update)
+    x.JsonSerializerOptions.DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull;
+});
+
+services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+
+// configure DI for application services
+services.AddScoped<IUserService, UserService>();
 
 // Swagger : génération automatique de la doc API
 services.AddEndpointsApiExplorer();
