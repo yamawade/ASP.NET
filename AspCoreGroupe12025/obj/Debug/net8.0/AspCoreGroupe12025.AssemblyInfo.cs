4340ba7cda0b082554deac06d55d50da49f90f86
yamawade <mariamawade69@gmail.com>
2025-05-31 02:41:02 +0000

    request

--- AspCoreGroupe12025/obj/Debug/net8.0/AspCoreGroupe12025.AssemblyInfo.cs
+++ AspCoreGroupe12025/obj/Debug/net8.0/AspCoreGroupe12025.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AspCoreGroupe12025")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8a266bf27d1cea64786ba57815918498eaa78e71")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+951db72a5f5fc6c7f622b8f17eb1ef0e8758f8af")]
 [assembly: System.Reflection.AssemblyProductAttribute("AspCoreGroupe12025")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AspCoreGroupe12025")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
