dcb2ccd1277c3f96f478721931da011e89be742b
yamawade <mariamawade69@gmail.com>
2025-05-31 03:10:41 +0000

    swagger

--- AspCoreGroupe12025/obj/Debug/net8.0/AspCoreGroupe12025.AssemblyInfo.cs
+++ AspCoreGroupe12025/obj/Debug/net8.0/AspCoreGroupe12025.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AspCoreGroupe12025")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+951db72a5f5fc6c7f622b8f17eb1ef0e8758f8af")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+07bd6f12ad3f3bf513118e307ac69f7190dc5ce9")]
 [assembly: System.Reflection.AssemblyProductAttribute("AspCoreGroupe12025")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AspCoreGroupe12025")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
